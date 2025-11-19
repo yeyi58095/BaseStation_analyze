adff8938385ac574e8f64d5079ce2ab6b337a04b
yeyi58095 <yeyi58095@gmail.com>
2025-11-19 13:02:45 +0800

    the test.sh is created for showing log to check whether the behavior is correct

---
+++ test.sh
@@ -0,0 +1,6 @@
+../Projects/BaseStation/Win32/Debug/Project2.exe \
+  --headless --T=200 --seed=1 --N=2 --C=100 --mu=1.25 --e=1.0 \
+  --Q=10 --tau=0.4 --rtx=1 --slots=0 --alwaysCharge \
+  --useD=1 --dDist=uniform --dP1=1 --dP2=10 \
+  --version=BaseStation --policy=rr \
+  --lambda=0.05 --out=Paper2/Fig6/out/test_uniform.csv --log=human
